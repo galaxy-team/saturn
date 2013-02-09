6b8eadb44dbde7b5f78b1ee140031cf7d2157a16
David Beckley <beckl.d.413@isg.edu.sa>
2013-02-10 01:58:23 +0300

    Fix test macro

--- src/test/basic_opcodes.cpp
+++ src/test/basic_opcodes.cpp
@@ -1,36 +1,38 @@
-#include <libjupiter.hpp>
+#include <libsaturn.hpp>
 #include <cstdint>
 #include <iostream>
 
 #define TEST_CASE(name, code, result) \
 std::vector<std::uint16_t> name(code); \
-dcpu name_cpu(); \
-name_cpu.flash(name.begin(), name.end()); \
+galaxy::saturn::dcpu name##_cpu; \
+name##_cpu.flash(name.begin(), name.end()); \
 try { \
     while(true) { \
-        name_cpu.cycle(); \
+        name##_cpu.cycle(); \
     } \
 } \
-catch(std::exception e) {} \
-if(name_cpu.A != result) { \
-    std::cerr << "error: Test case \"" << #name << "\" failed! It returned 0x" << std::hex << name_cpu.A << std::endl; \
+catch(std::exception& e) {} \
+if(name##_cpu.A != result) { \
+    std::cerr << "error: Test case \"" << #name << "\" failed! It returned 0x" << std::hex << name##_cpu.A << std::endl; \
     return -1; \
 } else { \
     std::cout << "Test case \"" << #name << "\" passed." << std::endl; \
 }
 
+#define PR(...) __VA_ARGS__
+
 
 int main() {
 
-    TEST_CASE(set, {0x7c01, 0xdead}, 0xdead);
-    TEST_CASE(add, {0xd001, 0x7c02, 0x0043}, 0x56);
-    TEST_CASE(sub, {0x7c03, 0xfffe}, 0x2);
-    TEST_CASE(mul, {0x7c01, 0x342f, 0x7c21, 0x0023, 0x0404}, 0x226d);
-    TEST_CASE(mli, {0x7c01, 0x00fe, 0xc005}, 0xee2);
-    TEST_CASE(div, {0x7c01, 0xffdb, 0x9806}, 0x332b);
-    TEST_CASE(dvi, {0x7c01, 0xffdb, 0x9807}, 0xfff9);
-    TEST_CASE(mod, {0x7c01, 0xffdb, 0x9808}, 0x4};
-    TEST_CASE(mdi, {0x7c01, 0xffdb, 0x9809}, 0xfffe};
+    TEST_CASE(set, PR({0x7c01, 0xdead}), 0xdead);
+    TEST_CASE(add, PR({0xd001, 0x7c02, 0x0043}), 0x56);
+    TEST_CASE(sub, PR({0x7c03, 0xfffe}), 0x2);
+    TEST_CASE(mul, PR({0x7c01, 0x342f, 0x7c21, 0x0023, 0x0404}), 0x226d);
+    TEST_CASE(mli, PR({0x7c01, 0x00fe, 0xc005}), 0xee2);
+    TEST_CASE(div, PR({0x7c01, 0xffdb, 0x9806}), 0x332b);
+    TEST_CASE(dvi, PR({0x7c01, 0xffdb, 0x9807}), 0xfff9);
+    TEST_CASE(mod, PR({0x7c01, 0xffdb, 0x9808}), 0x4);
+    TEST_CASE(mdi, PR({0x7c01, 0xffdb, 0x9809}), 0xfffe);
 
     /*
         TEST_CASE(and, {0x7c01, 
