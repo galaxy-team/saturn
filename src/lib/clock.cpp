d15f68d292639123d9212ce35b35d057ca4ce5d4
David Beckley <beckl.d.413@isg.edu.sa>
2013-02-20 22:05:23 +0300

    Started working on clock

---
+++ src/lib/clock.cpp
@@ -0,0 +1,67 @@
+/*
+
+This file is part of libsaturn.
+
+libsaturn is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+libsaturn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libsaturn.  If not, see <http://www.gnu.org/licenses/>.
+
+Your copy of the GNU Lesser General Public License should be in the
+file named "LICENSE-LGPL.txt".
+
+*/
+
+#include <clock.hpp>
+
+void galaxy::saturn::clock::interrupt()
+{
+    switch(cpu->A) {
+        /**
+         * The B register is read, and the clock will tick 60/B times per second.
+         * If B is 0, the clock is turned off.
+         */
+        case 0:
+            period = cpu->B;
+            ticks = 0;
+            break;
+
+        /**
+         * Store number of ticks elapsed since last call to 0 in C register
+         */
+        case 1:
+            cpu->C = ticks;
+            break;
+
+        /**
+         * If register B is non-zero, turn on interrupts with message B. If B is zero,
+         * disable interrupts
+         */
+        case 2:
+            if (cpu->B != 0) {
+                interrupt_message = cpu->B;
+
+            } else {
+                interrupt_message = 0x0;
+            }
+    }
+}
+
+void galaxy::saturn::clock::cycle()
+{
+    if (cycles++ >= ((period / 60) / (1 / clock_speed))) {
+        cycles = 0;
+        ticks++;
+        if (interrupt_message != 0) {
+            cpu->interrupt(interrupt_message);
+        }
+    }
+}
--- src/lib/clock.hpp
+++ src/lib/clock.hpp
@@ -36,12 +36,22 @@ namespace galaxy {
                 /// the clock ticks at 60/period times per second
                 std::uint16_t period;
 
-                /// stores the number of ticks
+                /// stores the number of ticks since the period was set
                 std::uint16_t ticks;
 
+                /// if this is nonzero, an interrupt with this message will be triggered on each tick
+                std::uint16_t interrupt_message;
+
+                /// stores the number of cycles since last tick
+                int cycles;
+
             public:
+                /// clock speed of the dcpu (perhaps this should be protected and constant? depends on if the client should be able to change it)
+                int clock_speed;
+
                 /// initialize the device to values specified by the spec
-                clock() : device(0x12d0b402, 0x0, 0x1, "Generic Clock (compatible)"), period(0x0) {}
+                clock() : device(0x12d0b402, 0x0, 0x1, "Generic Clock (compatible)"), period(0x0), ticks(0x1),
+                    interrupt_message(0x0), cycles(0), clock_speed(100000) {}
 
                 virtual void interrupt();
                 virtual void cycle();
