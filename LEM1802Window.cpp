783612ceaf23adca2fad2e33d0813229b063c112
David Beckley <beckl.d.413@isg.edu.sa>
2013-05-08 12:23:55 +0300

    Create LEM1802Window.cpp

---
+++ LEM1802Window.cpp
@@ -0,0 +1,28 @@
+/*
+
+This file is part of saturn.
+
+saturn is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+saturn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with saturn.  If not, see <http://www.gnu.org/licenses/>.
+
+Your copy of the GNU General Public License should be in the
+file named "LICENSE.txt".
+
+*/
+
+#include "LEM1802Window.hpp"
+
+void LEM1802Window::update()
+{
+    //pass
+}
