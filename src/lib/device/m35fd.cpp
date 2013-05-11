d4d8bd85f9cd8570c85060ee22c47829e293e289
Dominic May <jack.thatch@gmail.com>
2013-05-11 22:40:17 +0800

    Added naive skeleton for m35fd

---
+++ src/lib/device/m35fd.cpp
@@ -0,0 +1,75 @@
+/*
+
+This file is part of libsaturn.
+
+libsaturn is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+libsaturn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libsaturn.  If not, see <http://www.gnu.org/licenses/>.
+
+Your copy of the GNU Lesser General Public License should be in the
+file named "LICENSE-LGPL.txt".
+
+*/
+
+#include <libsaturn.hpp>
+#include <device.hpp>
+#include <m35fd.hpp>
+
+
+void galaxy::saturn::m35fd::interrupt()
+{
+    switch(cpu->A) {
+        /**
+         * Poll device;
+         * Sets B to the current state (see below) and C to the last error
+         * since the last device poll.
+         */
+        case 0:
+            // TODO 
+            break;
+
+        /**
+         * Set interupt;
+         * Enables interrupts and sets the message to X if X is anything
+         * other than 0, disables interrupts if X is 0. When interrupts are enabled,
+         * the M35FD will trigger an interrupt on the DCPU-16 whenever the state or
+         * error message changes.
+         */
+        case 1:
+            // TODO
+            break;
+
+        /**
+         * Read sector
+         * Reads sector X to DCPU ram starting at Y.
+         * Sets B to 1 if reading is possible and has been started, anything else if it
+         * fails. Reading is only possible if the state is STATE_READY or
+         * STATE_READY_WP.
+         * Protects against partial reads.
+         */
+        case 2:
+            // TODO
+            break;
+
+        /**
+         * Write sector;
+         * Writes sector X from DCPU ram starting at Y.
+         * Sets B to 1 if writing is possible and has been started, anything else if it
+         * fails. Writing is only possible if the state is STATE_READY.
+         * Protects against partial writes.
+         */
+        case 3:
+            // TODO
+            break;
+    }
+}
+
---
+++ src/lib/device/m35fd.hpp
@@ -0,0 +1,97 @@
+/*
+
+This file is part of libsaturn.
+
+libsaturn is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+libsaturn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libsaturn.  If not, see <http://www.gnu.org/licenses/>.
+
+Your copy of the GNU Lesser General Public License should be in the
+file named "LICENSE-LGPL.txt".
+
+*/
+
+#ifndef M35FD_HPP
+#define M35FD_HPP
+
+#include <libsaturn.hpp>
+#include <device.hpp>
+
+#include <cstdint>
+
+namespace galaxy {
+    namespace saturn {
+        /**
+         * represents a m35fd hardware device
+         */
+        class M35FD : public device {
+        protected:
+
+            // TODO make these an enum class'
+            enum fd_states {
+                STATE_NO_MEDIA, // There's no floppy in the drive.
+                STATE_READY,    // The drive is ready to accept commands.
+                STATE_READY_WP, // Same as ready, except the floppy is write protected.
+                STATE_BUSY      // The drive is busy either reading or writing a sector.
+            };
+
+            enum error_codes {
+                ERROR_NONE,       // There's been no error since the last poll.
+                ERROR_BUSY,       // Drive is busy performing an action.
+                ERROR_NO_MEDIA,   // Attempted to read or write with no floppy inserted.
+                ERROR_PROTECTED,  // Attempted to write to write protected floppy.
+                ERROR_EJECT,      // The floppy was removed while reading or writing.
+                ERROR_BAD_SECTOR, // The requested sector is broken, the data on it is lost.
+                ERROR_BROKEN      // There's some major software or hardware problem,
+                                  // try turning off and turning the device on again.
+            };
+
+            /// points to the 384-word segment of video ram
+            std::uint16_t vram_pointer;
+
+            /// points to the 256-word segment of font ram ram
+            std::uint16_t fram_pointer;
+
+            /// points to the 16-word segment of palette ram
+            std::uint16_t pram_pointer;
+
+            /// the index of the border color in the palette
+            std::uint8_t border_color;
+
+            /// when this is true, cells set to blink will be displayed, when it is false, they won't be displayed
+            bool blink_on;
+
+            /// holds the current state of the monitor
+            int state;
+
+            /// number of cycles since blink was last changed
+            std::uint32_t cycles;
+
+            /// the default font of the LEM1802
+            static const std::array<std::uint16_t, 256> default_font;
+
+            /// the default palette of the LEM1802
+            static const std::array<std::uint16_t, 16> default_palette;
+
+        public:
+            /// initialize the device to values specified by the spec
+/*            m35fd() : device(0x7349f615, 0x1c6c8b36, 0x1802, "LEM1802 - Low Energy Monitor"),
+                        vram_pointer(0), fram_pointer(0), pram_pointer(0), border_color(0),
+                        blink_on(false), state(DISCONNECTED), cycles(0) {}
+*/
+            virtual void interrupt();
+            virtual void cycle();
+        };
+    }
+}
+
+#endif
