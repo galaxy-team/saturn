59e7fa87c7e06e79522b2edf382246cbb7f0c1b2
Dominic May <jack.thatch@gmail.com>
2013-05-12 00:50:29 +0800

    better.

--- src/lib/libsaturn.cpp
+++ src/lib/libsaturn.cpp
@@ -668,7 +668,10 @@ std::uint16_t& galaxy::saturn::dcpu::get_reference(std::uint16_t val, bool a_val
 galaxy::saturn::device& galaxy::saturn::dcpu::attach_device(device* hw)
 {
     if (execution_has_begun)
-        throw "attach_device called after executation has begun";
+    {
+        std::cout << "attach_device called after executation has begun" << std::endl;
+        return -1; 
+    }
 
     devices.push_back(std::move(std::unique_ptr<device>(hw)));
     hw->cpu = this;
