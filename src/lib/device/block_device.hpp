3b3dd0538d63474e61485de1f5b11cbf87aec58d
Miles Rout <miles.rout@gmail.com>
2013-05-15 23:27:35 +1200

    Data members can't be virtual!

--- src/lib/device/block_device.hpp
+++ src/lib/device/block_device.hpp
@@ -9,9 +9,8 @@ namespace galaxy {
 //        namespace device {
             class block_device {
             protected:
-                virtual const int BLOCK_SIZE = 737280;
-                virtual std::array<std::uint16_t, BLOCK_SIZE> block_image;
-
+                static const int BLOCK_SIZE = 737280;
+                std::array<std::uint16_t, BLOCK_SIZE> block_image;
             public:
 //                void write_out_image(std::array<uint16_t> image);
                 void read_in_image(char* file_data_array, int image_filesize);
