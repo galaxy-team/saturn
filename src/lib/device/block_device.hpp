a725b4df708ddca878cad196970e0dba289e1ab5
Dominic May <jack.thatch@gmail.com>
2013-05-17 07:16:35 +0800

    A-cursed compilation errors

--- src/lib/device/block_device.hpp
+++ src/lib/device/block_device.hpp
@@ -10,12 +10,12 @@ namespace galaxy {
         template <int BlockSize>
         class block_device {
         protected:
-  //        virtual const int get_block_size() = 0;
-//          virtual std::array<std::uint16_t, 0> get_block_image() = 0;
+    //        virtual const int get_block_size() = 0;
+            virtual std::array<std::uint16_t, BlockSize> get_block_image() = 0;
         public:
             const static int BLOCK_SIZE = BlockSize;
 
-//          void write_out_image(std::array<uint16_t, BLOCK_SIZE> image);
+            void write_out_image(std::array<uint16_t, BLOCK_SIZE> image);
             void read_in_image(char* file_data_array, int image_filesize);
         };
     }
