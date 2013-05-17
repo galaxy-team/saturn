c69a8785b530630bb6a3bc825b933714962ba1c7
Dominic May <jack.thatch@gmail.com>
2013-05-17 20:08:09 +0800

    -.-

--- src/lib/device/block_device.hpp
+++ src/lib/device/block_device.hpp
@@ -14,7 +14,7 @@ namespace galaxy {
             const static int BLOCK_SIZE = BlockSize;
             virtual std::array<std::uint16_t, BlockSize> get_block_image() = 0;
 
-            void write_out_image(std::array<uint16_t, BLOCK_SIZE> image);
+//            void write_out_image(std::array<uint16_t, BLOCK_SIZE> image);
             void read_in_image(char* file_data_array, int image_filesize);
         };
     }
