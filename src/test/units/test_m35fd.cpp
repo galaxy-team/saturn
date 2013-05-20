acc4e0739b16553e0a7c0b0866c735aab1595860
Dominic May <jack.thatch@gmail.com>
2013-05-21 07:30:09 +0800

    More work on m35fd tests

--- src/test/units/test_m35fd.cpp
+++ src/test/units/test_m35fd.cpp
@@ -3,14 +3,6 @@
 #include <m35fd.hpp>
 #include <iostream>
 
-int get_floppy_id(galaxy::saturn::dcpu cpu) {
-    const std::vector<std::unique_ptr<galaxy::saturn::device>> devices = cpu.get_devices();
-    for (std::vector<std::unique_ptr<galaxy::saturn::device>>::size_type slot=0; slot < devices.size(); slot++) {
-        std::cout << "Slot; " << slot << std::endl;
-    }
-    // TODO
-}
-
 TEST_CASE("hardware/m35fd/initialisation", "test m35fd initailisation") {
     galaxy::saturn::dcpu cpu;
     galaxy::saturn::m35fd& m35fd = static_cast<galaxy::saturn::m35fd&>(cpu.attach_device(new galaxy::saturn::m35fd()));
@@ -32,13 +24,44 @@ TEST_CASE("hardware/m35fd/write_to_image", "test the write to disk mechanism; ba
 TEST_CASE("hardware/m35fd/write_to_floppy_disk", "test writing to floppy disk through assembly :D") {
     galaxy::saturn::dcpu cpu;
     galaxy::saturn::m35fd& m35fd = static_cast<galaxy::saturn::m35fd&>(cpu.attach_device(new galaxy::saturn::m35fd()));
+    /*
+    std::vector<std::uint16_t> codez;
+    for (int i=0; i<512; i++) {
+        codez.push_back(i);
+    }
+    cpu.flash(codez.begin(), codez.end());
+*/
 }
 
 TEST_CASE("hardware/m35fd/read_from_floppy_disk", "test reading from floppy disk through assembly :P") { 
     galaxy::saturn::dcpu cpu;
     galaxy::saturn::m35fd& m35fd = static_cast<galaxy::saturn::m35fd&>(cpu.attach_device(new galaxy::saturn::m35fd()));
-    int slot = get_floppy_slot(cpu);
-    REQUIRE(0!=1);
+
+    char * buffer = new char[512];
+    for (int i=0; i<512; i++) {
+        buffer[i] = i;
+    }
+    m35fd.read_in_image(buffer, 512);
+    m35fd.current_state = 0x1; // set state to STATE_READY
+
+    // Tell the floppy to read a sector from X to ram at Y
+    cpu.A = 2;
+    cpu.X = 0; // sector 0
+    cpu.Y = 0;
+    m35fd.interrupt();
+}
+
+TEST_CASE("hardware/m35fd/test_default_state", "tests the default state of the floppy") {
+    galaxy::saturn::dcpu cpu;
+    galaxy::saturn::m35fd& m35fd = static_cast<galaxy::saturn::m35fd&>(cpu.attach_device(new galaxy::saturn::m35fd()));
+
+    bool good = true;
+    for (int i=0; i < m35fd.BLOCK_SIZE; i++) {
+        if (!m35fd.block_image[i] == 0x0) {
+            good = false;
+        }
+    }
+    REQUIRE(good);
 }
 
 #endif
