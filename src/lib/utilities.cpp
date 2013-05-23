d9029329e2b7a9e1a0eb417b8284fbd6e7f1eab1
Dominic May <jack.thatch@gmail.com>
2013-05-23 18:25:54 +0800

    We want to keep the ammount of platform specific code to a minimum. Unforunately, this means we have to complicate some stuff ever so slightly

--- src/lib/utilities.cpp
+++ src/lib/utilities.cpp
@@ -3,15 +3,9 @@
 #include <array>
 #include <utilities.hpp>
 #include <fstream>
-#include <unistd.h>
 
 #ifndef __UTILITIES_CPP
 #define __UTILITIES_CPP
-
-/*bool galaxy::saturn::utilities::is_read_only(std::string filename){
-    char const* r_name = filename.c_str();
-    return access(r_name, W_OK);
-}*/
 
 std::vector<std::uint16_t> galaxy::saturn::utilities::string_to_vector(char * string) {
     
